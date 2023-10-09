d783974d9c35929c0a9e34f259b08fd965845f23
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-10-09 18:42:47 +0200

    Register user functionality

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -3,12 +3,13 @@ const API_URL_REGISTER = `${API_URL_BASE}auth/register`;
 const API_URL_LOGIN = `${API_URL_BASE}auth/login`;
 const API_URL_POSTS = `${API_URL_BASE}posts`;
 // To create a new post, use API_URL_POSTS and make a POST request. 
+const id = "";
 const API_URL_PROFILE_POSTS = `${API_URL_BASE}profiles/${username}/posts`;
 const API_URL_SINGLE_POST = `${API_URL_BASE}posts/${id}`;
 // To edit a post, use API_URL_SINGLE_POST and make a PUT request. 
 // To delete a post, use API_URL_SINGLE_POST and make a DELETE request.
 
-export {
+export const API_URLS = {
     API_URL_BASE,
     API_URL_LOGIN,
     API_URL_POSTS,
