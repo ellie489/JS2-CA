28314d9af3d8cc0129fcbf1d5adf37921cf75480
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-10-15 22:44:26 +0200

    clean up constants

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -2,20 +2,12 @@ const BASE = "https://api.noroff.dev/api/v1/social/";
 const REGISTER = `${BASE}auth/register`;
 const LOGIN = `${BASE}auth/login`;
 const POSTS = `${BASE}posts`;
-// To create a new post, use API_URL_POSTS and make a POST request. 
-const id = "";
-
-
-//const API_URL_PROFILE_POSTS = `${API_URL_BASE}profiles/${username}/posts`;
-//const API_URL_SINGLE_POST = `${API_URL_BASE}posts/${id}`;
-// To edit a post, use API_URL_SINGLE_POST and make a PUT request. 
-// To delete a post, use API_URL_SINGLE_POST and make a DELETE request.
+const USER_POSTS = `${BASE}profiles/`
 
 export const API_URLS = {
     BASE,
     LOGIN,
     POSTS,
-   // API_URL_PROFILE_POSTS,
+    USER_POSTS,
     REGISTER,
-  //  API_URL_SINGLE_POST
 };
