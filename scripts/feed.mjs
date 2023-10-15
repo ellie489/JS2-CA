b8c83cf03f270e7c1ddd3da7146fcb85e5037b29
ellie489 <114486930+ellie489@users.noreply.github.com>
2023-10-15 22:44:42 +0200

    edit post functionality

--- scripts/feed.mjs
+++ scripts/feed.mjs
@@ -1,8 +1,8 @@
 import { fetchData } from "./doFetch.mjs";
 import { API_URLS } from "./constants.mjs";
 
-function createPostElement(post) {
-    const { id, title, body, media } = post;
+export function createPostElement(post) {
+    const { title, body, media } = post;
 
     const postContainer = document.createElement("div");
     postContainer.classList.add("card", "mb-3");
@@ -11,7 +11,6 @@ function createPostElement(post) {
     if (media) {
         const postImage = createImageElement(media, "card-img-top");
 
-        // Add an error handler to remove the image if it fails to load
         postImage.onerror = function () {
             postContainer.removeChild(postImage);
         };
@@ -33,28 +32,19 @@ function createPostElement(post) {
 }
 
 function populatePostsContainer(postsData) {
-    const postsWithImages = postsData.filter((post) => post.media);
-
-    if (postsWithImages.length === 0) {
-        // Optionally, you can handle the case where no posts with images are found.
-        // For example, you can display a message or hide the container.
-        return;
-    }
-
     const postsContainer = document.getElementById("posts-container");
-
-    // Clear any existing content in the container
-    postsContainer.innerHTML = "";
-
-    // Loop through the posts data with images and create HTML for each post
-    postsWithImages.forEach((post) => {
-        const postElement = createPostElement(post);
-        const postId = post.id;
-        postElement.addEventListener("click", createPostClickHandler(post, postId));
-
-        postsContainer.appendChild(postElement);
+    postsContainer.innerHTML = ""; // Clear any existing content
+    postsData.forEach((post) => {
+        if (post.media) {
+            // Check if the post has a valid media URL
+            const postElement = createPostElement(post);
+            const postId = post.id;
+            postElement.addEventListener("click", createPostClickHandler(post, postId));
+            postsContainer.appendChild(postElement);
+        }
     });
 }
+
 
 // Function to create an image element with a source and class
 function createImageElement(src, className) {
@@ -81,17 +71,20 @@ function appendChildren(parent, children) {
     children.forEach((child) => parent.appendChild(child));
 }
 
-// Fetch posts and call the populatePostsContainer function
+// ...
+
+
 async function fetchPosts() {
     const accessToken = localStorage.getItem("accessToken");
 
     if (!accessToken) {
-        window.location.href = "/user/login.html";
+        window.location.href = "../login";
         return;
     }
 
     try {
-        const postsData = await fetchData(accessToken, API_URLS.POSTS);
+        const limit = 100;
+        const postsData = await fetchData(accessToken, `${API_URLS.POSTS}/?limit=${limit}`);
         console.log("Posts data:", postsData);
 
         // Call the function to populate the posts container
@@ -108,5 +101,5 @@ function createPostClickHandler(post, postId) {
     };
 }
 
-// Attach an event listener to load and fetch/display posts when the page loads
 window.addEventListener("load", fetchPosts);
+
